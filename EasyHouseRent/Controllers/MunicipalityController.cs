134ccd67ad7cf2aad9fdecf9489d6a905d471710
Mjhon03 <Mjhon1841@gmail.com>
2022-04-04 13:25:39 -0500

    first commit

---
+++ EasyHouseRent/Controllers/MunicipalityController.cs
@@ -0,0 +1,74 @@
+﻿using EasyHouseRent.Model;
+using EasyHouseRent.Model.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EasyHouseRent.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MunicipalityController : ControllerBase
+    {
+        BaseData db = new BaseData();
+        // GET: api/<MunicipalityController>
+        [HttpGet]
+        public IEnumerable<Municipios> Get()
+        {
+            string sql = "SELECT * FROM municipios WHERE nombre != 'desconocido'";
+            DataTable dt = db.getTable(sql);
+            List<Municipios> MunicipioList = new List<Municipios>();
+            MunicipioList = (from DataRow dr in dt.Rows
+                         select new Municipios()
+                         {
+                             idmunicipio = Convert.ToInt32(dr["idmunicipio"]),
+                             nombre = dr["nombre"].ToString(),
+                             departamento = Convert.ToInt32(dr["departamento"]),
+                         }).ToList();
+
+            return MunicipioList;
+        }
+
+        // GET api/<MunicipalityController>/nombreDepartamento
+        [HttpGet("{iddepartamento}")]
+        [Route("api/Municipality/{iddepartamento}")]
+        public IEnumerable<Municipios>Get([FromQuery]int iddepartamento)
+        {
+            string sql = $"select m.* FROM municipios m INNER JOIN departamento d on m.departamento=d.iddepartamento where d.iddepartamento = {iddepartamento}";
+            DataTable dt = db.getTable(sql);
+            List<Municipios> MunicipioList = new List<Municipios>();
+            MunicipioList = (from DataRow dr in dt.Rows
+                             select new Municipios()
+                             {
+                                 idmunicipio = Convert.ToInt32(dr["idmunicipio"]),
+                                 nombre = dr["nombre"].ToString(),
+                                 departamento = Convert.ToInt32(dr["departamento"]),
+                             }).ToList();
+            return MunicipioList;
+        }
+
+        // POST api/<MunicipalityController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+
+        // PUT api/<MunicipalityController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MunicipalityController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ EasyHouseRent/Controllers/UsersController.cs
@@ -0,0 +1,86 @@
+﻿using EasyHouseRent.Model;
+using EasyHouseRent.Model.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EasyHouseRent.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // GET: api/<UsersController>
+        BaseData db = new BaseData(); 
+        [HttpGet]
+        public IEnumerable<Usuarios> Get([FromQuery] Usuarios user)
+        {
+            string sql = $"SELECT * FROM usuarios where email = '{user.email}' and contraseña = '{Encrypt.GetSHA256(user.contraseña)}'";
+            DataTable dt = db.getTable(sql);
+            List<Usuarios> usersList = new List<Usuarios>();
+            usersList = (from DataRow dr in dt.Rows
+                         select new Usuarios()
+                         {
+                             idusuario = Convert.ToInt32(dr["idusuario"]),
+                             nombre = dr["nombre"].ToString(),
+                             apellidos = dr["apellidos"].ToString(),
+                             edad = Convert.ToInt32(dr["edad"]),
+                             telefono = dr["telefono"].ToString(),
+                             email = dr["email"].ToString(),
+                             contraseña = dr["contraseña"].ToString(),
+                             estado = dr["estado"].ToString(),
+                             departamento = Convert.ToInt32(dr["departamento"]),
+                             municipio = Convert.ToInt32(dr["municipio"])
+
+                         }).ToList();
+
+            return usersList;
+
+
+
+        }
+        //https://localhost:44352/api/Users?email=juancito&contrase%C3%B1a=jhoncito
+
+        // GET api/<UsersController>/5
+        [HttpGet("{email}/{password}")]
+        public void Get( )
+        {
+           
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public string Post([FromBody] Usuarios user)
+        {
+            //Insertar usuario
+            string sql = "INSERT INTO usuarios (nombre,apellidos,edad,telefono,email,contraseña,estado,departamento,municipio) VALUES ('" + user.nombre + "','" + user.apellidos + "','" + user.edad + "','" + user.telefono + "','" + user.email + "','" + Encrypt.GetSHA256(user.contraseña) + "','" + user.estado + "','" + user.departamento + "','" + user.municipio + "');";
+            string result = db.executeSql(sql);
+            return result;
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut]
+        public string Put([FromBody] Usuarios user)
+        {
+            //Actualizar datos del Usuario
+            string sql = "UPDATE usuarios SET nombre = '" + user.nombre + "', apellidos = '" + user.apellidos + "', edad = '" + user.edad + "', telefono ='" + user.telefono + "', email ='" + user.email + "', contraseña ='" + user.contraseña + "', estado ='" + user.estado + "', departamento ='" + user.departamento + "', municipio ='" + user.municipio + "'  WHERE idusuario = '" + user.idusuario + "'";
+            string resultado = db.executeSql(sql);
+            return resultado;
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete]
+        public string Delete([FromBody] Usuarios user)
+        {
+            //Eliminar Usuario
+            string sql = "DELETE FROM usuarios WHERE idusuario = " + user.idusuario;
+            string result = db.executeSql(sql);
+            return result;
+        }
+    }
+}
